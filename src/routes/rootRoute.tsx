6804be690f4ef5f952a612aee3c78854a51395e0
Yaroslav <BeletskiiIE@lad24.ru>
2021-09-03 13:26:08 +0300

    delete Main Icon

--- src/routes/rootRoute.tsx
+++ src/routes/rootRoute.tsx
@@ -42,18 +42,6 @@ const RootRoute = () => {
         <div className='router'>
         <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container-xxl">
-          <a className="navbar-brand" href="/main">App</a>
-              <button
-                className="navbar-toggler"
-                type="button"
-                disabled
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-              >
-            <span className="navbar-toggler-icon"></span>
-          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   {links.map((link, index) => (
